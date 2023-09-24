f278cd358f5b7123f47e359b4b7508e3390f0860
Ronit Sarkar <iamrony777@proton.me>
2023-09-24 15:50:19 +0000

    Sun, Sep 24, 2023, 9:20 PM +05:30

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ echo -e "Installing poetry using official installer"
 curl -sSL https://install.python-poetry.org | python -
 
 echo -e "Updating PATH"
-echo "export PATH=$POETRY_HOME/bin:$PATH"
+echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
+
+exec $SHELL && poetry --version
